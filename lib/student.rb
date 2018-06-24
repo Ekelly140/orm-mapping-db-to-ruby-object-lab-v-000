2e00f6e6d1c7442be73574aa086503fca0b83d58
ERIC KELLY <eakelly220@gmail.com>
2018-06-24 07:09:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,6 +54,18 @@ class Student
    end
   end
 
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE grade < 12
+   SQL
+
+     DB[:conn].execute(sql).map do |row|
+     self.new_from_db(row)
+   end
+  end
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
