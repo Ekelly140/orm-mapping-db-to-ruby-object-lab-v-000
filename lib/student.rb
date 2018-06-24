600723e1a998257819df4722485e6ef00a074132
ERIC KELLY <eakelly220@gmail.com>
2018-06-24 04:15:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
   end
 
-  def count_all_students_in_grade_9
+  def self.count_all_students_in_grade_9
     sql = <<-SQL
      SELECT *
      FROM students
